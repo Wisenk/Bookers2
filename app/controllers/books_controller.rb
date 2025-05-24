1735d48abd102bbb1fb4065733fa82c9450641df
Wisenk <wisenkmr93@gmail.com>
2025-05-24 16:36:43 +0000

    [in-progress] Bookers 2 - 16行

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,39 @@
+class BooksController < ApplicationController
+
+  def new
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-root to: "homes#top"
+root 'homes#top'
+resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
 get "homes/about" => "homes#about", as: "about"
 end
