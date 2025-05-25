ccef44b805af29ce74fdc0d41eb5f999c9bbcd90
Wisenk <wisenkmr93@gmail.com>
2025-05-25 17:17:50 +0000

    [Finish]Bookers2 - 25行

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ devise_for :users
 resources :books, only: [:edit, :index, :show, :new, :create]
 resources :users, only: [:show, :edit]
 
-get "top" => "homes#top", as: "top"
 get "homes/about" => "homes#about", as: "about"
 end
