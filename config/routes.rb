4123f190ec9cecdf8383b6e9116cc16e87aa9541
Wisenk <wisenkmr93@gmail.com>
2025-05-27 14:29:41 +0000

    [finish]Bookers2 - 28行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root to: 'homes#top'
+root to: 'homes#top', as: 'root'
 devise_for :users
 resources :books, only: [:edit, :index, :show, :new, :create]
 resources :users, only: [:show, :edit]
