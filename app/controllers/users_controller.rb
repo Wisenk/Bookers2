beeb06f022086be192e23340274530aa819f3a3a
Wisenk <wisenkmr93@gmail.com>
2025-06-01 09:04:03 +0000

    [finish]Bookers2 - 46行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,15 @@ class UsersController < ApplicationController
 
 
   def show
-    @books = Book.all
+    @user = User.find(params[:id])
+    @books = @user.books
     @book = Book.new
   end
   
   def index
-    @user = current_user
     @users = User.all
-    @books = Book.all
+    @user = current_user
+    @books = @user.books
     @book = Book.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 root to: 'homes#top', as: 'root'
 devise_for :users
-resources :books, only: [:edit, :index, :show, :create, :update, :destroy, :new]
-resources :users, only: [:index, :show, :edit, :update, :destroy]
+resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+resources :users, only: [:index, :show, :edit, :update]
 get "homes/about" => "homes#about", as: "about"
 
 end
