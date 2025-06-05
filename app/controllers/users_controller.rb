cc7d6c7dd16fa7ab34758bc53e44f3e8e72b19f6
Wisenk <wisenkmr93@gmail.com>
2025-06-05 17:30:34 +0000

    [finish rspec]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     if @user != current_user
       redirect_to user_path(current_user)
   end
+end
       
   def update
     @user = User.find(params[:id])
